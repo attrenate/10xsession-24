261c0d01cee70ffa29b97867d265458d4084db6d
attrenate <nikolozghomidzevol100@gmail.com>
2025-03-25 18:11:35 +0400

    initial commit

---
+++ session24.js
@@ -0,0 +1,15 @@
+// 1. Batman’s Night Patrol (If Statements & Boolean Conditions)
+function batmanPatrol(signalOn){
+    if(signalOn){
+        console.log('Batman is on patrol!');
+     } else {
+         console.log('Gotham is quiet tonight');
+    }
+}
+batmanPatrol(true);
+batmanPatrol(false);
+
+// 2. Sorting Avengers (Truthy & Falsy Values)
+function availableAvengers(avengers){
+    
+}
